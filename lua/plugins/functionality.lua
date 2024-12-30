dc19745891f5ee36e10700752d8313130798a442
Rendell Ngo <sand77x@gmail.com>
2024-12-30 11:01:41 +0800

    better undotree settings

--- lua/plugins/functionality.lua
+++ lua/plugins/functionality.lua
@@ -1,6 +1,13 @@
 return {
 	{ "tpope/vim-commentary" },
-	{ "mbbill/undotree" },
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.g.undotree_SplitWidth = 35
+			vim.g.undotree_DiffpanelHeight = 15
+			vim.g.undotree_WindowLayout = 2
+		end,
+	},
 	{ "wellle/targets.vim" },
 	{
 		"ggandor/leap.nvim",
